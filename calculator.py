0334fb3f37a29d5d65c53f33dabd9aade62c2507
erling <nupen.erling@gmail.com>
2023-08-31 17:41:59 +0200

    added divide, factorial, sin, mean, and var test functions, and with three different test-sets

--- calculator.py
+++ calculator.py
@@ -1,5 +1,24 @@
-
+import math
+import statistics
 
 def add(x: float, y: float) -> float:
     return x + y
 
+def divide(x,y):
+    return x/y
+
+def factorial(x):
+    return math.factorial(x)
+
+def sin(x):
+    N = 20
+    for i in range(N):
+        sine = 0
+        sine += (-1)**i * (x**(2*N + 1))/(math.factorial(2*N + 1))
+        return sine
+
+def mean(x): #must have list as input
+    return statistics.mean(x)
+
+def var(x): #must have list as input
+    return statistics.variance(x)
--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 
 import pytest
-from calculator import add
+from calculator import add, divide, factorial, sin, mean, var
 
 #print(add(1,2))
 
@@ -17,3 +17,32 @@ def test_float(test_input1, test_input2 , expected):
     assert abs(add(test_input1,test_input2) - expected) < tol
     
 
+@pytest.mark.parametrize("test_input1, test_input2, expected",[(1,2,0.5), (2,3,5),(3,4,7)])
+
+def test_divide(test_input1, test_input2, expected):
+    assert divide(test_input1,test_input2) == expected
+
+
+@pytest.mark.parametrize("test_input1, expected",[(10,100), (2,3),(3,7)])
+
+def test_factorial(test_input1,  expected):
+    assert factorial(test_input1) == expected
+
+@pytest.mark.parametrize("test_input1, expected",[(0,0), (0.2,0.3),(0.3,0.7)])
+
+def test_sin(test_input1,expected):
+    assert sin(test_input1) == expected
+
+x_list1 = [1,2,3,4,5,6,7,2,4,56,7]
+x_list2 = [2,3,4,6,7,4,5,76,4,]
+x_list3 = [2,3,45,3,2,4,3,5,6,4,4,3,5]
+
+@pytest.mark.parametrize("test_input1, expected",[(x_list1, 5), (x_list2,4),(x_list3,4)])
+
+def test_mean(test_input1, expected):
+    assert mean(test_input1) == expected
+
+@pytest.mark.parametrize("test_input1, expected",[(x_list1, 5), (x_list2,4),(x_list3,4)])
+                         
+def test_var(test_input1, expected):
+    assert var(test_input1) == expected
